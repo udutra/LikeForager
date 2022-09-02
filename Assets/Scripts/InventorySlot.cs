458cf636e70bff125759a5438e9036be440019a3
udutra <udutra@gmail.com>
2022-09-02 17:47:58 -0300

    Inventário p3.
    10 - INVENTÁRIO - PARTE 3

---
+++ Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour {
+
+    private Item item;
+    public Image itemImage;
+    public Text amountText;
+
+    public void UpdateSlot(Item i, int amount) {
+        item = i;
+        itemImage.sprite = item.itemSprite;
+        amountText.text = amount.ToString();
+    }
+
+    public void OnSlotClick() {
+
+    }
+
+    public void MouseEnter() {
+
+    }
+
+    public void MouseExit() {
+
+    }
+}
