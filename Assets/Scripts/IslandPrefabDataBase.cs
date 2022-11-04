4b1555b8aa2ec38c49083a06b50e3307af0c71d0
udutra <udutra@gmail.com>
2022-11-04 18:41:48 -0300

    Ajustes dos recursos por nível
    21 - APRIMORAMENTO DOS RECURSOS POR NÍVEL

--- Assets/Scripts/IslandPrefabDataBase.cs
+++ Assets/Scripts/IslandPrefabDataBase.cs
@@ -5,28 +5,57 @@ using UnityEngine;
 public class IslandPrefabDataBase : MonoBehaviour {
     public ResourceLoot[] resourceLV1;
     public ResourceLoot[] resourceLV2;
+    public ResourceLoot[] resourceLV3;
+    public ResourceLoot[] resourceLV4;
+    public ResourceLoot[] resourceLV5;
+    public int[] levelToupgrade;
     public List<GameObject> resourceIsland = new List<GameObject>();
 
     public void CreateIslandResources() {
         resourceIsland.Clear();
 
-        if (resourceLV1.Length > 0) {
-            foreach (ResourceLoot l in resourceLV1) {
+        for (int i = 0; i < levelToupgrade.Length; i++) {
+            if (CoreGame._instance.gameManager.playerLevel >= levelToupgrade[i]) {
+
+                switch (i) {
+
+                    case 0: {
+                            ResourceLevel(resourceLV1);
+                            break;
+                        }
+                    case 1: {
+                            ResourceLevel(resourceLV2);
+                            break;
+                        }
+                    case 2: {
+                            ResourceLevel(resourceLV3);
+                            break;
+                        }
+                    case 3: {
+                            ResourceLevel(resourceLV4);
+                            break;
+                        }
+                    case 4: {
+                            ResourceLevel(resourceLV5);
+                            break;
+                        }
+                    default: {
+                            break;
+                        }
+                }
+            }
+
+        }
+    }
+
+    private void ResourceLevel(ResourceLoot[] res) {
+        if (res.Length > 0) {
+            foreach (ResourceLoot l in res) {
                 for (int i = 0; i < l.amount; i++) {
                     resourceIsland.Add(l.resource);
                 }
             }
         }
-
-        if (CoreGame._instance.gameManager.playerLevel >= 2) {
-            if (resourceLV2.Length > 0) {
-                foreach (ResourceLoot l in resourceLV2) {
-                    for (int i = 0; i < l.amount; i++) {
-                        resourceIsland.Add(l.resource);
-                    }
-                }
-            }
-        }
     }
-    
+
 }
